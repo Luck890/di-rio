d338806ae8f2d00a4b0781ae9b0fa369d13a1add
Luck890 <145017207+Luck890@users.noreply.github.com>
2024-11-26 23:50:26 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -7,29 +7,29 @@ criaCartao(
 criaCartao(
     'Custo da Viagem', 
     'Qual foi o custo total dessa viagem por pessoa e qual a modalidade de aquisição?', 
-    'o custo foi entre R$3100,00 na passagem aérea (CAD750,09 em dólar canadense), aquisição via agência.'
+    'o custo foi entre R$3100,00 na passagem aérea (CAD750,09 em dólar canadense), aquisição via agência.',
 )
 
 criaCartao(
     'Fato Curioso', 
     'Uma curiosidade sobre cidade/ponto turístico/país visitado!', 
-    'Uma das características do Canadá mais conhecidas é o fato de ser multicultural, inclusive, o país conta com diversas políticas de imigração que beneficiam os estrangeiros.'
+    'Uma das características do Canadá mais conhecidas é o fato de ser multicultural, inclusive, o país conta com diversas políticas de imigração que beneficiam os estrangeiros.',
 
     
 criaCartao(
     'Passeios', 
     'Onde passear?', 
-    'Além de apenas conhecer a cidade, lá possui passeios como o museu Casa Loma, o aquário Ripley Aquarium of Canada e a Galeria de Arte de Ontário.'
+    'Além de apenas conhecer a cidade, lá possui passeios como o museu Casa Loma, o aquário Ripley Aquarium of Canada e a Galeria de Arte de Ontário.',
 )
 
 criaCartao(
     'Prato Típico', 
     'Conhece o "nome do prato"?', 
-    'Peameal Bacon, é um tipo de bacon canadense, feito de lombo de porco curado e enrolado em farinha de milho, conhecido por sua textura suculenta e sabor levemente adociado.'
+    'Peameal Bacon, é um tipo de bacon canadense, feito de lombo de porco curado e enrolado em farinha de milho, conhecido por sua textura suculenta e sabor levemente adociado.',
 )
 
 criaCartao(
     'Ponto de Vista', 
     'O que eu achei da viagem para ...?', 
-    'a viagem até Toronto foi uma das melhores viagens ja feitas na minha vida, poder conhecer outra cultura, os pontos turísticos anteriormente mencionados, mas com certeza a próxima vez irei tentar ficar por mais tempo, pois duas semanas por incrível foi pouco para conseguir explorar os pontos turísticos da cidade.'
-)
+    'a viagem até Toronto foi uma das melhores viagens ja feitas na minha vida, poder conhecer outra cultura, os pontos turísticos anteriormente mencionados, mas com certeza a próxima vez irei tentar ficar por mais tempo, pois duas semanas por incrível foi pouco para conseguir explorar os pontos turísticos da cidade.',
+),
