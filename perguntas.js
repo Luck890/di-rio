5d079db914c63e332c6840892514dffe0c2e98b9
Luck890 <145017207+Luck890@users.noreply.github.com>
2024-11-26 23:56:15 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,35 @@
 criaCartao(
     'Diário de Bordo', 
     'Qual foi o destino?', 
-    'O destino selecionado foi Toronto, Canadá', 
+    'Informar o destino da viagem.', 
 )
 
 criaCartao(
     'Custo da Viagem', 
     'Qual foi o custo total dessa viagem por pessoa e qual a modalidade de aquisição?', 
-    'o custo foi entre R$3100,00 na passagem aérea (CAD750,09 em dólar canadense), aquisição via agência.',
+    'Informar o valor na moeda original do local e o valor convertido para R$. Informar se a aquisição foi via agência de viagens ou aquisição de forma independente '
 )
 
 criaCartao(
     'Fato Curioso', 
     'Uma curiosidade sobre cidade/ponto turístico/país visitado!', 
-    'Uma das características do Canadá mais conhecidas é o fato de ser multicultural, inclusive, o país conta com diversas políticas de imigração que beneficiam os estrangeiros.',
+    'Informar através de um breve texto uma curiosidade.'
+)
 
-    
 criaCartao(
     'Passeios', 
     'Onde passear?', 
-    'Além de apenas conhecer a cidade, lá possui passeios como o museu Casa Loma, o aquário Ripley Aquarium of Canada e a Galeria de Arte de Ontário.',
+    'Informar os locais, breve resumo do que são.'
 )
 
 criaCartao(
     'Prato Típico', 
     'Conhece o "nome do prato"?', 
-    'Peameal Bacon, é um tipo de bacon canadense, feito de lombo de porco curado e enrolado em farinha de milho, conhecido por sua textura suculenta e sabor levemente adociado.',
+    'Explicar sobre o prato, porque o escolheu e o ingrediente principal.'
 )
 
 criaCartao(
     'Ponto de Vista', 
     'O que eu achei da viagem para ...?', 
-    'a viagem até Toronto foi uma das melhores viagens ja feitas na minha vida, poder conhecer outra cultura, os pontos turísticos anteriormente mencionados, mas com certeza a próxima vez irei tentar ficar por mais tempo, pois duas semanas por incrível foi pouco para conseguir explorar os pontos turísticos da cidade.',
-),
+    'Informar através de um breve texto sobre o que achou da viagem, o que faria de diferente e por quê.'
+)
